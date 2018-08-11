81dd8c737d763e99d73e9386d3dad6f25c95fbb3
Kevin Sun <cykevinsun@gmail.com>
2018-08-11 16:14:32 -0700

    Initial Commit

---
+++ GoBang/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GoBang
+//
+//  Created by Kevin Sun on 2018/8/11.
+//  Copyright © 2018年 Kevin Sun. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
