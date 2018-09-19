2265e5eeffb8b421d9d8bedf7710d967e120da4d
Kevin Sun <cykevinsun@gmail.com>
2018-09-19 01:32:54 -0700

    reset

--- GoBang/main.cpp
+++ GoBang/main.cpp
@@ -12,12 +12,6 @@
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
-
-    std::unique_ptr<int> a;
-    a.reset(new int(5));
-    std::cout << *a.get() << std::endl;
-    a.reset(new int(6));
-    std::cout << *a.get() << std::endl;
     
     MainWindow window;
     window.show();
